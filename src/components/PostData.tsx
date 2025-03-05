7f5f0ea3e76b55f52f6c26f920c1d4cea47a9ab9
mostafamahmoud11911 <mm0766633@gmail.com>
2025-03-05 23:02:29 +0400

    update validation

--- src/components/PostData.tsx
+++ src/components/PostData.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { usePosts } from "../context/PostContext";
 
@@ -7,28 +7,58 @@ export default function PostData() {
   const { state } = useLocation();
   const edit = state?.isEdit;
   const id = state?.post?.id;
-  const { handleCreateAndUpdate,spinner } = usePosts();
+  const { handleCreateAndUpdate, spinner } = usePosts();
+  const [titleValidation, setTitleValidation] = useState("");
+  const [bodyValidation, setbodyValidation] = useState("");
+
 
   const navigate = useNavigate();
 
+  const [data, setData] = useState({ title: '', body: '' });
 
-  const title = useRef<HTMLInputElement | null>(null);
-  const description = useRef<HTMLTextAreaElement | null>(null);
+  const getData = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const cloneData = { ...data };
+    setData({ ...cloneData, [e.target.name]: e.target.value });
+  }
 
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const data = {
-      title: title.current?.value,
-      body: description.current?.value
+  const validation = () => {
+    if (!data.title || !new RegExp(/^[A-Za-z1-9]{3,50}$/).test(data.title)) {
+      setTitleValidation('Title is not valid');
+      return false;
+    } else if (!data.body || !new RegExp(/^[A-Za-z1-9]{10,200}$/).test(data.body)) {
+      setbodyValidation('Description is not valid');
+      return false;
+    } else {
+      setTitleValidation('');
+      setbodyValidation('');
+      return true
     }
-    const response: unknown = await handleCreateAndUpdate(data, id, edit);
-
-    if (response) navigate('/');
 
   }
 
 
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = validation();
+    console.log(result);
+
+    if (!result) {
+      return false;
+    } else {
+      const info = {
+        title: data.title,
+        body: data.body
+      }
+      const response: unknown = await handleCreateAndUpdate(info, id, edit);
+
+      if (response) navigate('/');
+    }
+
+  }
+
+console.log(data);
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
@@ -38,11 +68,13 @@ export default function PostData() {
       <form onSubmit={onSubmit} className="md:w-[600px] w-full mx-auto flex flex-col gap-4 px-3">
         <div className="flex flex-col">
           <label htmlFor="title">Title</label>
-          <input type="text" ref={title} defaultValue={edit ? state?.post?.title : null} className="border-2 border-gray-600 rounded-md px-2 py-1 " placeholder="title" />
+          <input type="text" onChange={getData}  name="title" defaultValue={edit ? state?.post?.title : null} className="border-2 border-gray-600 rounded-md px-2 py-1 " placeholder="title" />
+          <p className="text-red-500 ">{titleValidation}</p>
         </div>
         <div className="flex flex-col">
           <label htmlFor="description">Description</label>
-          <textarea name="description" className="border-2 border-gray-600 rounded-md px-2 py-1 " defaultValue={edit ? state?.post?.body : null} ref={description} id="description" ></textarea>
+          <textarea name="body" onChange={getData}  className="border-2 border-gray-600 rounded-md px-2 py-1 " defaultValue={edit ? state?.post?.body : null} id="description" ></textarea>
+          <p className="text-red-500">{bodyValidation}</p>
         </div>
         <button type="submit" disabled={spinner} className={`w-full bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer ${spinner && 'opacity-50'}`}>{edit ? 'Update' : 'Create'}</button>
         <Link to="/" className="w-full text-center bg-gray-300 text-black px-4 py-2 rounded-md cursor-pointer">Cancel</Link>
